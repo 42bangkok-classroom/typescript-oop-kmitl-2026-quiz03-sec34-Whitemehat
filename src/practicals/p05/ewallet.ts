47bf2d77386a9b43619e14dd7b8d57c27276db2a
Whitemehat <wong1234.pe@gmail.com>
2026-03-24 19:08:23 +0700

    practice

--- src/practicals/p05/ewallet.ts
+++ src/practicals/p05/ewallet.ts
@@ -7,7 +7,7 @@ export class EWallet extends PaymentMethod{
         this.walletId = wallet;
     }
     processPayment(amount: number): boolean {
-        console.log(`Cutting e-wallet balance:${amount} Baht`)
+        console.log(`Cutting e-wallet balance: ${amount} Baht`)
         return true;
     }
     getPaymentFee(amount: number): number {
